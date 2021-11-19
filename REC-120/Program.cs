578e0db17d119a72f37542f9683b633cef40badc
сергей токарев <sseregatokarew@gmail.com>
2021-11-19 23:41:18 +0700

    Добавьте файлы проекта.

---
+++ REC-120/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace REC_120
+{
+    class Program
+    {
+        static string NotReplace(char[] str)
+        {
+            for (int i = 0; i < str.Length; i++)
+                if (str[i] == '.')
+                    str[i] = '!';
+            String NewStr = new String(str);
+            return NewStr;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            char[] str = Console.ReadLine().ToCharArray();
+            Console.Write($"Новая строка: {NotReplace(str)}");
+            Console.ReadKey();
+        }
+    }
+}
